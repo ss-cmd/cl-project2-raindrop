9162d90e6ba62909dc080c10a77592b14ceebd07
ss-cmd <66662667+ss-cmd@users.noreply.github.com>
2020-11-18 19:48:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ app.use('/', express.static('public'));
 const users = {}
 
 //Initialize the actual HTTP server
-
-let http = require('http');
-let server = http.createServer(app);
 let port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log("Server listening at port: " + port);
+server.listen(port, ()=> {
+console.log('listening at ', port);
 });
 
 //Initialize socket.io
